21b35df737103cd95f8ef5be0077357aa8a26ff6
Jingci Li <jingcikeili@gmail.com>
2021-01-29 19:08:47 -0500

    update navigation

--- app/src/main/java/com/lyapunov/tetris/StartActivity.kt
+++ app/src/main/java/com/lyapunov/tetris/StartActivity.kt
@@ -1,11 +1,34 @@
 package com.lyapunov.tetris
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val newGameButton = findViewById<Button>(R.id.NewGameButton)
+        val settingButton = findViewById<Button>(R.id.settingButton)
+        val highscoreButton = findViewById<Button>(R.id.HighScoreButton)
+
+        newGameButton.setOnClickListener {
+            val intent = Intent(this@StartActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        settingButton.setOnClickListener {
+            val intent = Intent(this@StartActivity, SettingActivity::class.java)
+            startActivity(intent)
+        }
+
+        highscoreButton.setOnClickListener {
+            val intent = Intent(this@StartActivity, HighscoreActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
