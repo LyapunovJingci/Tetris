b2fe4d08c21e3f17a3fc0a0a8c8eda36a964d82b
Jingci Li <jingcikeili@gmail.com>
2021-02-07 14:48:50 -0500

    update setting UI logic

--- app/src/main/java/com/lyapunov/tetris/SettingActivity.kt
+++ app/src/main/java/com/lyapunov/tetris/SettingActivity.kt
@@ -2,10 +2,38 @@ package com.lyapunov.tetris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
+import com.lyapunov.tetris.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySettingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.levelInstant.text = (progress + 1).toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        binding.radioTheme1.setOnClickListener { Log.d("checked", "theme1") }
+        binding.radioTheme2.setOnClickListener { Log.d("checked", "theme2") }
+        binding.radioTheme3.setOnClickListener { Log.d("checked", "theme3") }
+        binding.radioTheme4.setOnClickListener { Log.d("checked", "theme4") }
+        binding.radioTheme5.setOnClickListener { Log.d("checked", "theme5") }
     }
 }
+
